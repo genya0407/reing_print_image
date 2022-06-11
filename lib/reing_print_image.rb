2aa222803f13a81760c93c477df9f38f558067ed
Yusuke Sangenya <longinus.eva@gmail.com>
2022-06-11 18:23:07 +0900

    init

---
+++ lib/reing_print_image.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "reing_print_image/version"
+
+module ReingPrintImage
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/reing_print_image_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ReingPrintImage do
+  it "has a version number" do
+    expect(ReingPrintImage::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
